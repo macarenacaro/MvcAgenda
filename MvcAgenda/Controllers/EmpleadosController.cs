2a45f984defc3bcbf7eaff8c1c1ad7bd8752a891
Macarena Caro <maccarcor@alu.edu.gva.es>
2023-11-20 15:51:22 +0100

    pagina20 fin

---
+++ MvcAgenda/Controllers/EmpleadosController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MvcAgenda.Data;
+using MvcAgenda.Models;
+
+namespace MvcAgenda.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private readonly MvcAgendaContexto _context;
+
+        public EmpleadosController(MvcAgendaContexto context)
+        {
+            _context = context;
+        }
+
+        // GET: Empleados
+        public async Task<IActionResult> Index()
+        {
+            var mvcAgendaContexto = _context.Empleados.Include(e => e.Departamento);
+            return View(await mvcAgendaContexto.ToListAsync());
+        }
+
+        // GET: Empleados/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Empleados == null)
+            {
+                return NotFound();
+            }
+
+            var empleado = await _context.Empleados
+                .Include(e => e.Departamento)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+
+            return View(empleado);
+        }
+
+        // GET: Empleados/Create
+        public IActionResult Create()
+        {
+            ViewData["DepartamentoId"] = new SelectList(_context.Departamentos, "Id", "Nombre");
+            return View();
+        }
+
+        // POST: Empleados/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre,FechaNacimiento,DepartamentoId")] Empleado empleado)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(empleado);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DepartamentoId"] = new SelectList(_context.Departamentos, "Id", "Nombre", empleado.DepartamentoId);
+            return View(empleado);
+        }
+
+        // GET: Empleados/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Empleados == null)
+            {
+                return NotFound();
+            }
+
+            var empleado = await _context.Empleados.FindAsync(id);
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+            ViewData["DepartamentoId"] = new SelectList(_context.Departamentos, "Id", "Nombre", empleado.DepartamentoId);
+            return View(empleado);
+        }
+
+        // POST: Empleados/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,FechaNacimiento,DepartamentoId")] Empleado empleado)
+        {
+            if (id != empleado.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(empleado);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmpleadoExists(empleado.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DepartamentoId"] = new SelectList(_context.Departamentos, "Id", "Nombre", empleado.DepartamentoId);
+            return View(empleado);
+        }
+
+        // GET: Empleados/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Empleados == null)
+            {
+                return NotFound();
+            }
+
+            var empleado = await _context.Empleados
+                .Include(e => e.Departamento)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+
+            return View(empleado);
+        }
+
+        // POST: Empleados/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Empleados == null)
+            {
+                return Problem("Entity set 'MvcAgendaContexto.Empleados'  is null.");
+            }
+            var empleado = await _context.Empleados.FindAsync(id);
+            if (empleado != null)
+            {
+                _context.Empleados.Remove(empleado);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmpleadoExists(int id)
+        {
+          return (_context.Empleados?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ MvcAgenda/Controllers/TareasController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MvcAgenda.Data;
+using MvcAgenda.Models;
+
+namespace MvcAgenda.Controllers
+{
+    public class TareasController : Controller
+    {
+        private readonly MvcAgendaContexto _context;
+
+        public TareasController(MvcAgendaContexto context)
+        {
+            _context = context;
+        }
+
+        // GET: Tareas
+        public async Task<IActionResult> Index()
+        {
+            var mvcAgendaContexto = _context.Tareas.Include(t => t.Empleado);
+            return View(await mvcAgendaContexto.ToListAsync());
+        }
+
+        // GET: Tareas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Tareas == null)
+            {
+                return NotFound();
+            }
+
+            var tarea = await _context.Tareas
+                .Include(t => t.Empleado)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tarea == null)
+            {
+                return NotFound();
+            }
+
+            return View(tarea);
+        }
+
+        // GET: Tareas/Create
+        public IActionResult Create()
+        {
+            ViewData["EmpleadoId"] = new SelectList(_context.Empleados, "Id", "Nombre");
+            return View();
+        }
+
+        // POST: Tareas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Descripcion,FechaInicio,FechaFin,HoraInicio,HoraFin,EmpleadoId")] Tarea tarea)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tarea);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmpleadoId"] = new SelectList(_context.Empleados, "Id", "Nombre", tarea.EmpleadoId);
+            return View(tarea);
+        }
+
+        // GET: Tareas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Tareas == null)
+            {
+                return NotFound();
+            }
+
+            var tarea = await _context.Tareas.FindAsync(id);
+            if (tarea == null)
+            {
+                return NotFound();
+            }
+            ViewData["EmpleadoId"] = new SelectList(_context.Empleados, "Id", "Nombre", tarea.EmpleadoId);
+            return View(tarea);
+        }
+
+        // POST: Tareas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Descripcion,FechaInicio,FechaFin,HoraInicio,HoraFin,EmpleadoId")] Tarea tarea)
+        {
+            if (id != tarea.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tarea);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TareaExists(tarea.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmpleadoId"] = new SelectList(_context.Empleados, "Id", "Nombre", tarea.EmpleadoId);
+            return View(tarea);
+        }
+
+        // GET: Tareas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Tareas == null)
+            {
+                return NotFound();
+            }
+
+            var tarea = await _context.Tareas
+                .Include(t => t.Empleado)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tarea == null)
+            {
+                return NotFound();
+            }
+
+            return View(tarea);
+        }
+
+        // POST: Tareas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Tareas == null)
+            {
+                return Problem("Entity set 'MvcAgendaContexto.Tareas'  is null.");
+            }
+            var tarea = await _context.Tareas.FindAsync(id);
+            if (tarea != null)
+            {
+                _context.Tareas.Remove(tarea);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TareaExists(int id)
+        {
+          return (_context.Tareas?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
